0a7a4741bac0c54d4de3915cc50a48e1ce4d9ea5
D025449 <Rainer.Soltek@gmail.com>
2026-04-20 15:51:05 +0200

    newest2

--- ecosystem.config.sample.js
+++ ecosystem.config.sample.js
@@ -8,10 +8,10 @@ module.exports = {
                 PORT: 3000,
                 COGNITO_REGION: "eu-central-1",
                 COGNITO_USER_POOL_ID: "eu-central-1_FGuEMHRmT",
-                COGNITO_CLIENT_ID: "3pb6qu43hq57i1q6lup0fp25sk",
+                COGNITO_CLIENT_ID: "23bskjfka86t95qofg11f6gi4b",
                 //COGNITO_CLIENT_SECRET: "",
                 COGNITO_DOMAIN: "https://eu-central-1fguemhrmt.auth.eu-central-1.amazoncognito.com",
-                COGNITO_REDIRECT_URI: "https://cwa24.de/landing",
+                COGNITO_REDIRECT_URI: "https://cwa24.de/auth/callback",
                 APP_BASE_URL: "https://cwa24.de",
                 SESSION_SECRET: "your-prod-session-secret",
                 // PostgreSQL
