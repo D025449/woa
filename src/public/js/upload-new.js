12a82db8b3c13831308b7adfa72b947401634030
D025449 <Rainer.Soltek@gmail.com>
2026-06-17 18:18:05 +0200

    nnn

--- src/public/js/upload-new.js
+++ src/public/js/upload-new.js
@@ -530,6 +530,6 @@ function escapeHtml(value) {
         .replace(/&/g, "&amp;")
         .replace(/</g, "&lt;")
         .replace(/>/g, "&gt;")
-        .replace(/\"/g, "&quot;")
+        .replace(/"/g, "&quot;")
         .replace(/'/g, "&#39;");
 }
